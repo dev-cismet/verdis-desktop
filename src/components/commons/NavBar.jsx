4f213eb1a764d229b9056c243c2e46b3b25df8e1
David Glogaza <david.glogaza40@googlemail.com>
2023-07-18 17:07:58 +0200

    issue #002 add changeable size & remove unused components

--- src/components/commons/NavBar.jsx
+++ src/components/commons/NavBar.jsx
@@ -47,70 +47,60 @@ const NavBar = ({
   const data = extractor(dataIn);
 
   return (
-    <Layout>
-      <Layout.Header
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          backgroundColor: "#FFFFFF",
-        }}
-        width={width}
-        height={height}
-      >
-        <div className="flex items-center gap-3">
-          <FontAwesomeIcon icon={faGripVertical} className="w-6 h-6" />
-          <img src={Logo} alt="Logo" className="h-10" />
-          {data.map((link, i) => (
-            <Button
-              type="text"
-              key={`navLink_${i}`}
-              className={`${i === 0 ? "text-primary" : ""} font-semibold`}
-              href={link.href}
-            >
-              {link.title}
-            </Button>
-          ))}
-        </div>
-        <div className="flex items-center gap-3 w-1/4">
-          <Input
-            placeholder="Suche..."
-            addonBefore={
-              <FontAwesomeIcon icon={faSearch} className="cursor-pointer" />
-            }
-            allowClear
-          />
-          <FontAwesomeIcon
-            icon={faCirclePlus}
-            className="w-6 h-6 cursor-pointer"
-          />
-        </div>
-        <div className="flex items-center gap-3">
-          <FontAwesomeIcon
-            icon={faCircleExclamation}
-            className="w-6 h-6 cursor-pointer"
-          />
-          <FontAwesomeIcon
-            icon={faCircleQuestion}
-            className="w-6 h-6 cursor-pointer"
-          />
-          <FontAwesomeIcon
-            icon={faComment}
-            className="w-6 h-6 cursor-pointer"
-          />
-          <FontAwesomeIcon
-            icon={faArrowRightFromBracket}
-            className="w-6 h-6 cursor-pointer"
-          />
-          <Avatar
-            size="large"
-            icon={<FontAwesomeIcon icon={faUser} />}
-            className="cursor-pointer"
-          />
-          <Button type="text">Einstellungen</Button>
-        </div>
-      </Layout.Header>
-    </Layout>
+    <Layout.Header
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#FFFFFF",
+        width,
+        height,
+      }}
+    >
+      <div className="flex items-center gap-3">
+        <FontAwesomeIcon icon={faGripVertical} className="w-6 h-6" />
+        <img src={Logo} alt="Logo" className="h-10" />
+        {data.map((link, i) => (
+          <Button
+            type="text"
+            key={`navLink_${i}`}
+            className={`${i === 0 ? "text-primary" : ""} font-semibold`}
+            href={link.href}
+          >
+            {link.title}
+          </Button>
+        ))}
+      </div>
+      <div className="flex items-center gap-3 w-1/4">
+        <Input
+          placeholder="Suche..."
+          addonBefore={
+            <FontAwesomeIcon icon={faSearch} className="cursor-pointer" />
+          }
+          allowClear
+        />
+      </div>
+      <div className="flex items-center gap-3">
+        <FontAwesomeIcon
+          icon={faCircleExclamation}
+          className="w-6 h-6 cursor-pointer"
+        />
+        <FontAwesomeIcon
+          icon={faCircleQuestion}
+          className="w-6 h-6 cursor-pointer"
+        />
+        <FontAwesomeIcon icon={faComment} className="w-6 h-6 cursor-pointer" />
+        <FontAwesomeIcon
+          icon={faArrowRightFromBracket}
+          className="w-6 h-6 cursor-pointer"
+        />
+        <Avatar
+          size="large"
+          icon={<FontAwesomeIcon icon={faUser} />}
+          className="cursor-pointer"
+        />
+      </div>
+    </Layout.Header>
   );
 };
 
