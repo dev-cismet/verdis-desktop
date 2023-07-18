b9f11e9c78a7af23230753567c75bfb3bae72c14
David Glogaza <david.glogaza40@googlemail.com>
2023-07-18 20:53:23 +0200

    issue #002 fix missing tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#E67843",
+    extend: {
+      colors: {
+        primary: "#E67843",
+      },
     },
   },
   plugins: [],
