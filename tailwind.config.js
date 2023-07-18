c866c6facf92a0b1880da7437b07662bbd19cd4e
David Glogaza <david.glogaza40@googlemail.com>
2023-07-18 15:07:31 +0200

    issue #002 remove duplicate theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#E67843",
+    },
   },
   plugins: [],
   corePlugins: {
     preflight: false,
-  },
-  theme: {
-    colors: {
-      primary: "#E67843",
-    },
   },
 };
