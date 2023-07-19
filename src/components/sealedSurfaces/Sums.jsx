4fe6876868f7b3f58fd4717becde361f0ab4913e
David Glogaza <david.glogaza40@googlemail.com>
2023-07-19 11:40:08 +0200

    issue #002 change sums list to simpler design

--- src/components/sealedSurfaces/Sums.jsx
+++ src/components/sealedSurfaces/Sums.jsx
@@ -1,57 +1,54 @@
-import { List } from "antd";
 import CustomCard from "../ui/Card";
 
 const mockExtractor = (input) => {
-  return {
-    dataSource: [
-      {
-        title: "Bewertung",
-        items: [
-          {
-            name: "710_DF",
-            area: 1234,
-          },
-          {
-            name: "720_DF",
-            area: 1,
-          },
-          {
-            name: "730_DF",
-            area: 12,
-          },
-          {
-            name: "740_DF",
-            area: 123,
-          },
-          {
-            name: "750_DF",
-            area: 456,
-          },
-          {
-            name: "760_DF",
-            area: 1734,
-          },
-          {
-            name: "770_DF",
-            area: 7567,
-          },
-        ],
-      },
-      {
-        title: "Grad der Korrektur",
-        items: [
-          {
-            name: "Connected",
-            area: 123,
-          },
-          {
-            name: "Sleeping",
-            area: 12345,
-          },
-        ],
-      },
-    ],
-  };
+  return [
+    {
+      title: "Bewertung",
+      items: [
+        {
+          name: "710_DF",
+          area: 1234,
+        },
+        {
+          name: "720_DF",
+          area: 1,
+        },
+        {
+          name: "730_DF",
+          area: 12,
+        },
+        {
+          name: "740_DF",
+          area: 123,
+        },
+        {
+          name: "750_DF",
+          area: 456,
+        },
+        {
+          name: "760_DF",
+          area: 1734,
+        },
+        {
+          name: "770_DF",
+          area: 7567,
+        },
+      ],
+    },
+    {
+      title: "Anschlussgrad",
+      items: [
+        {
+          name: "angeschlossen",
+          area: 123,
+        },
+        {
+          name: "versickernd",
+          area: 12345,
+        },
+      ],
+    },
+  ];
 };
 
 const Sums = ({
@@ -65,23 +62,25 @@ const Sums = ({
 
   return (
     <CustomCard style={{ ...style, width, height }} title="Summen">
-      <List
-        itemLayout="horizontal"
-        dataSource={data.dataSource}
-        renderItem={(data) => (
-          <>
-            <List.Item>
-              <List.Item.Meta title={data.title} />
-            </List.Item>
-            {data.items.map((item) => (
-              <List.Item>
-                <div>{item.name}</div>
-                <div>{item.area} m²</div>
-              </List.Item>
-            ))}
-          </>
-        )}
-      />
+      {data.map((categories, i) => (
+        <div
+          key={`sum_categories_${i}`}
+          className="flex flex-col gap-2 text-sm"
+        >
+          <div className={`font-semibold ${i > 0 && "pt-4"}`}>
+            {categories.title}
+          </div>
+          {categories.items.map((item, i) => (
+            <div
+              key={`sum_items_${i}`}
+              className="flex justify-between w-full items-center"
+            >
+              <div>{item.name}</div>
+              <div>{item.area} m²</div>
+            </div>
+          ))}
+        </div>
+      ))}
     </CustomCard>
   );
 };
