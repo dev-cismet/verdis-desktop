797d1d89fd827042c0f87b7dae9c6ae0a21f626a
David Glogaza <david.glogaza40@googlemail.com>
2023-07-24 08:40:11 +0200

    issue #002 add fronts table component

---
+++ src/components/streetCleaning/Fronts.jsx
@@ -0,0 +1,72 @@
+import { Table } from "antd";
+import CustomCard from "../ui/Card";
+
+const mockExtractor = (input) => {
+  return {
+    dataSource: [
+      {
+        key: "1",
+        number: 1,
+        length: 2,
+        class: "A4",
+      },
+      {
+        key: "2",
+        number: 2,
+        length: 55,
+        class: "A4",
+      },
+      {
+        key: "3",
+        number: 3,
+        length: 34,
+        class: "A4",
+      },
+      {
+        key: "4",
+        number: 4,
+        length: 22,
+        class: "A4",
+      },
+    ],
+    columns: [
+      {
+        title: "Nummer",
+        dataIndex: "number",
+        key: "number",
+      },
+      {
+        title: "Länge in m",
+        dataIndex: "length",
+        key: "length",
+      },
+      {
+        title: "Klasse",
+        dataIndex: "class",
+        key: "class",
+      },
+    ],
+  };
+};
+
+const Fronts = ({
+  dataIn,
+  extractor = mockExtractor,
+  width = 300,
+  height = 200,
+  style,
+}) => {
+  const data = extractor(dataIn);
+
+  return (
+    <CustomCard style={{ ...style, width, height }} title="Flächen">
+      <Table
+        dataSource={data.dataSource}
+        columns={data.columns}
+        pagination={{ position: ["none"] }}
+      />
+    </CustomCard>
+  );
+};
+
+export default Fronts;
