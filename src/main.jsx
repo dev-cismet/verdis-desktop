afeaac85cd3233d8d22cf5c1712233cee86679c4
David Glogaza <david.glogaza40@googlemail.com>
2023-08-07 13:33:13 +0200

    issue #002 change to react query

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ import SeepagePermitsDetailsPage from "./pages/SeepagePermitsDetails";
 import LoginPage from "./pages/Login";
 import NavBar from "./components/commons/NavBar";
 import { getJWT } from "./store/slices/auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const NavBarWrapper = () => {
   const jwt = useSelector(getJWT);
@@ -96,6 +97,8 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ConfigProvider
@@ -106,9 +109,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       }}
       locale={locale}
     >
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </QueryClientProvider>
     </ConfigProvider>
   </React.StrictMode>
 );
