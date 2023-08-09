5bba2f753d1a5f8bf8c37e0ac4644e5e22f876d9
David Glogaza <david.glogaza40@googlemail.com>
2023-08-09 18:21:48 +0200

    issue #002 update areas to use extractor

--- src/components/sealedSurfaces/Areas.jsx
+++ src/components/sealedSurfaces/Areas.jsx
@@ -1,39 +1,16 @@
 import { Table } from "antd";
 import CustomCard from "../ui/Card";
-import { DOMAIN, REST_SERVICE } from "../../constants/verdis";
 import { useSelector } from "react-redux";
-import { getJWT } from "../../store/slices/auth";
 import { getKassenzeichen } from "../../store/slices/search";
-import { useQuery } from "@tanstack/react-query";
-import request, { gql } from "graphql-request";
 
-const query = gql`
-  query Table($kassenzeichen: Int) {
-    kassenzeichen(where: { kassenzeichennummer8: { _eq: $kassenzeichen } }) {
-      flaechenArray(
-        order_by: {
-          flaecheObject: {
-            flaecheninfoObject: { flaechenbeschreibung: { beschreibung: asc } }
-          }
-        }
-      ) {
-        flaecheObject {
-          flaechenbezeichnung
-          flaecheninfoObject {
-            groesse_aus_grafik
-            flaechenbeschreibung {
-              beschreibung
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-const endpoint = REST_SERVICE + `/graphql/` + DOMAIN + "/execute";
-
-const mockExtractor = (input) => {
-  return [];
+const extractor = (input) => {
+  const data = input?.flaechenArray?.map((row) => ({
+    name: row?.flaecheObject?.flaechenbezeichnung,
+    size: row?.flaecheObject?.flaecheninfoObject?.groesse_aus_grafik,
+    type: row?.flaecheObject?.flaecheninfoObject?.flaechenbeschreibung
+      ?.beschreibung,
+  }));
+  return data;
 };
 
 const columns = [
@@ -55,42 +32,14 @@ const columns = [
   },
 ];
 
-const Areas = ({
-  dataIn,
-  extractor = mockExtractor,
-  width = 300,
-  height = 200,
-  style,
-}) => {
-  const mockData = extractor(dataIn);
-  const jwt = useSelector(getJWT);
+const Areas = ({ width = 300, height = 200, style }) => {
   const kassenzeichen = useSelector(getKassenzeichen);
-
-  const { data } = useQuery({
-    queryKey: ["detailsTable", kassenzeichen],
-    queryFn: async () =>
-      request(
-        endpoint,
-        query,
-        { kassenzeichen: kassenzeichen },
-        {
-          Authorization: `Bearer ${jwt}`,
-        }
-      ),
-    enabled: !!kassenzeichen,
-  });
-
-  const dataSource = data?.kassenzeichen[0]?.flaechenArray?.map((row) => ({
-    name: row?.flaecheObject?.flaechenbezeichnung,
-    size: row?.flaecheObject?.flaecheninfoObject?.groesse_aus_grafik,
-    type: row?.flaecheObject?.flaecheninfoObject?.flaechenbeschreibung
-      ?.beschreibung,
-  }));
+  const data = extractor(kassenzeichen);
 
   return (
     <CustomCard style={{ ...style, width, height }} title="Flächen">
       <Table
-        dataSource={dataSource}
+        dataSource={data}
         columns={columns}
         pagination={false}
         size="small"
