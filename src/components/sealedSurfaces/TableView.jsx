d4357e5193b0baea1ab2ae2188fea1959f840a23
David Glogaza <david.glogaza40@googlemail.com>
2023-08-09 18:22:16 +0200

    issue #002 update area table to use extractor

--- src/components/sealedSurfaces/TableView.jsx
+++ src/components/sealedSurfaces/TableView.jsx
@@ -1,51 +1,31 @@
 import { Table } from "antd";
 import CustomCard from "../ui/Card";
-import request, { gql } from "graphql-request";
 import { useDispatch, useSelector } from "react-redux";
-import { getJWT } from "../../store/slices/auth";
 import {
   getFlaechenId,
   getKassenzeichen,
   storeFlaechenId,
 } from "../../store/slices/search";
-import { useQuery } from "@tanstack/react-query";
-import { DOMAIN, REST_SERVICE } from "../../constants/verdis";
 
-const query = gql`
-  query DetailsTable($kassenzeichen: Int) {
-    kassenzeichen(where: { kassenzeichennummer8: { _eq: $kassenzeichen } }) {
-      flaechenArray(
-        order_by: {
-          flaecheObject: {
-            flaecheninfoObject: { flaechenbeschreibung: { beschreibung: asc } }
-          }
-        }
-      ) {
-        flaecheObject {
-          datum_erfassung
-          flaechenbezeichnung
-          flaecheninfoObject {
-            groesse_aus_grafik
-            flaechenartObject {
-              art_abkuerzung
-            }
-            anschlussgradObject {
-              grad_abkuerzung
-            }
-            flaechenbeschreibung {
-              beschreibung
-            }
-          }
-          id
-        }
-      }
-    }
-  }
-`;
-const endpoint = REST_SERVICE + `/graphql/` + DOMAIN + "/execute";
+const extractor = (input) => {
+  const data = input?.flaechenArray?.map((flaeche) => ({
+    name: flaeche?.flaecheObject?.flaechenbezeichnung,
+    size: flaeche?.flaecheObject?.flaecheninfoObject?.groesse_aus_grafik,
+    type: flaeche?.flaecheObject?.flaecheninfoObject?.flaechenartObject
+      ?.art_abkuerzung,
+    connection:
+      flaeche?.flaecheObject?.flaecheninfoObject?.anschlussgradObject
+        ?.grad_abkuerzung,
+    description:
+      flaeche?.flaecheObject?.flaecheninfoObject?.flaechenbeschreibung
+        ?.beschreibung,
+    date: flaeche?.flaecheObject?.datum_erfassung,
+    id: flaeche?.flaecheObject?.id,
+  }));
 
-const mockExtractor = (input) => {
-  return [];
+  console.log(data);
+
+  return data;
 };
 
 const columns = [
@@ -81,56 +61,20 @@ const columns = [
   },
 ];
 
-const TableView = ({
-  dataIn,
-  extractor = mockExtractor,
-  width = 300,
-  height = 200,
-  style,
-}) => {
-  const mockData = extractor(dataIn);
-  const jwt = useSelector(getJWT);
+const TableView = ({ width = 300, height = 200, style }) => {
   const kassenzeichen = useSelector(getKassenzeichen);
+  const data = extractor(kassenzeichen);
   const flaechenId = useSelector(getFlaechenId);
   const dispatch = useDispatch();
-
-  const { data } = useQuery({
-    queryKey: ["detailsTable", kassenzeichen],
-    queryFn: async () =>
-      request(
-        endpoint,
-        query,
-        { kassenzeichen: kassenzeichen },
-        {
-          Authorization: `Bearer ${jwt}`,
-        }
-      ),
-    enabled: !!kassenzeichen,
-  });
-
-  const dataSource = data?.kassenzeichen[0]?.flaechenArray?.map((row) => ({
-    name: row?.flaecheObject?.flaechenbezeichnung,
-    size: row?.flaecheObject?.flaecheninfoObject?.groesse_aus_grafik,
-    type: row?.flaecheObject?.flaecheninfoObject?.flaechenartObject
-      ?.art_abkuerzung,
-    connection:
-      row?.flaecheObject?.flaecheninfoObject?.anschlussgradObject
-        ?.grad_abkuerzung,
-    description:
-      row?.flaecheObject?.flaecheninfoObject?.flaechenbeschreibung
-        ?.beschreibung,
-    date: row?.flaecheObject?.datum_erfassung,
-    id: row?.flaecheObject?.id,
-  }));
 
   return (
     <CustomCard style={{ ...style, width, height }} title="Flächen">
       <Table
-        dataSource={dataSource}
+        dataSource={data}
         columns={columns}
         pagination={false}
         size="small"
-        onRow={(record, i) => {
+        onRow={(record) => {
           return {
             onClick: () => dispatch(storeFlaechenId(record.id)),
           };
