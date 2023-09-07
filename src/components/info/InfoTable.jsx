7cc3f92f78dcc9bb2b748106b486159fa9003b77
David Glogaza <david.glogaza40@googlemail.com>
2023-09-07 11:23:44 +0200

    issue #002 change colors of geometries

--- src/components/info/InfoTable.jsx
+++ src/components/info/InfoTable.jsx
@@ -1,32 +1,24 @@
 import { useSelector } from "react-redux";
 import CustomCard from "../ui/Card";
 import { getKassenzeichen } from "../../store/slices/search";
+import { LandParcelColors } from "../../tools/mappingTools";
 
 const extractor = (kassenzeichen) => {
   return kassenzeichen?.kassenzeichen_geometrienArray?.map((geometry) => ({
     title: geometry.kassenzeichen_geometrieObject.name,
-    status: "online",
+    id: geometry.id,
   }));
 };
 
-const Row = ({ title, status }) => {
-  const getStatusColor = (status) => {
-    switch (status) {
-      case "online":
-        return "#21FF37";
-      case "pending":
-        return "#FFCE21";
-      case "offline":
-        return "#FF0000";
-    }
-  };
+const Row = ({ title, id }) => {
+  const color = LandParcelColors[id % LandParcelColors.length];
 
   return (
     <>
       <div className="flex items-center w-full gap-3 px-2 text-base">
         <div
           className="w-2 h-2"
-          style={{ backgroundColor: getStatusColor(status) }}
+          style={{ backgroundColor: color, opacity: 0.6 }}
         ></div>
         <span>{title}</span>
       </div>
@@ -40,13 +32,14 @@ const Geometrics = ({
   height = 200,
   style,
   title = "Geometrien",
+  dataIn,
 }) => {
   const kassenzeichen = useSelector(getKassenzeichen);
-  const data = extractor(kassenzeichen);
+  const data = extractor(dataIn ? dataIn : kassenzeichen);
   return (
     <CustomCard style={{ ...style, width, height }} title={title}>
       {data?.map((row, i) => (
-        <Row key={`geometrics_${i}`} status={row.status} title={row.title} />
+        <Row key={`geometrics_${i}`} id={row.id} title={row.title} />
       ))}
     </CustomCard>
   );
