edc4360abaa0b1d751cc95cbb9a6cbd7284c65d6
Thorsten Hell <thorsten.hell@cismet.de>
2023-09-14 17:44:42 +0200

    just add the base again 🙈

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base_: "/verdis-desktop/",
+  base: "/verdis-desktop/",
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
