9cefcfff1097469e9090560a7cce10c0aa2cd6b2
David Glogaza <david.glogaza40@googlemail.com>
2023-09-18 14:05:05 +0200

    issue #002 add more information to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,52 @@
 import Login from "../components/authentication/Login";
+import packageJson from "../../package.json";
 
 const Page = () => {
   return (
     <div className="h-screen">
-      {/* <img
-        src="/images/background.jpg"
-        alt="Hintergrund Bild"
-        className="absolute z-0"
-      /> */}
-      <div className="w-full flex h-full items-center justify-center bg-rain">
+      <div className="w-full flex h-full items-center justify-center bg-rain relative bg-cover">
         <Login width="40%" height="50%" />
+        <img
+          src="/images/wuppertal-white.svg"
+          alt="Wupperwurm"
+          className="absolute top-6 left-6 w-40"
+        />
+        <div className="absolute top-6 right-6 text-white/80 font-semibold flex flex-col gap-2 items-end">
+          <span>Stadt Wuppertal</span>
+          <span>Vermessung, Katasteramt und Geodaten</span>
+          <span>102.23 Kommunalservice Liegenschaftskataster</span>
+        </div>
+        <div className="absolute bottom-6 right-6 text-white/80 font-semibold flex flex-col gap-2 items-end">
+          <span>
+            VerDIS Desktop v:{packageJson.version}{" "}
+            <a href="https://cismet.de" className="text-white/50 no-underline">
+              cismet GmbH
+            </a>{" "}
+            auf Basis von
+          </span>
+          <span>
+            <a
+              href="https://leafletjs.com/"
+              className="text-white/50 no-underline"
+            >
+              Leaflet
+            </a>{" "}
+            und{" "}
+            <a
+              href="https://cismet.de/#refs"
+              className="text-white/50 no-underline"
+            >
+              cids | react cismap v
+              {packageJson.dependencies["react-cismap"].slice(1)} |
+            </a>
+          </span>
+          <a
+            href="https://cismet.de/datenschutzerklaerung.html"
+            className="text-white/50 no-underline"
+          >
+            Datenschutzerklärung (Privacy Policy)
+          </a>
+        </div>
       </div>
     </div>
   );
