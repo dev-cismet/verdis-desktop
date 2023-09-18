88aea89b9e4dd2208bd5a571f8639d13ce459538
David Glogaza <david.glogaza40@googlemail.com>
2023-09-18 16:16:35 +0200

    issue #002 remove timeout from cross reference logic

--- src/components/overview/CrossReferences.jsx
+++ src/components/overview/CrossReferences.jsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import CustomCard from "../ui/Card";
+import { useDispatch } from "react-redux";
+import { setIsLoading } from "../../store/slices/search";
 
 const mockExtractor = (input) => {
   return [
@@ -26,20 +28,28 @@ const CrossReferences = ({
   style,
 }) => {
   const data = extractor(dataIn);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   return (
     <CustomCard style={{ ...style, width, height }} title="Querverweise">
       <div className="flex flex-col gap-1 items-center justify-center text-sm">
         {data.map((row, i) => (
-          <Link
+          <div
             key={`crossReferences_${i}`}
             className="flex w-full justify-center items-center py-1 hover:bg-zinc-100 cursor-pointer"
-            to={`/versiegelteFlaechen?kassenzeichen=${row.kassenzeichen}&bez=${row.bez}`}
+            onClick={() => {
+              dispatch(setIsLoading(true));
+              navigate(
+                `/versiegelteFlaechen?kassenzeichen=${row.kassenzeichen}&bez=${row.bez}`
+              );
+            }}
+            typeof="button"
           >
             <span className="text-black underline">
               {row.kassenzeichen}:{row.bez}
             </span>
-          </Link>
+          </div>
         ))}
       </div>
     </CustomCard>
