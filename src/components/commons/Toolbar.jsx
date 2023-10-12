0d7e6b1585ac3d2fedf606c24bed79e665119e38
David Glogaza <david.glogaza40@googlemail.com>
2023-10-12 12:34:59 +0200

    issue #027 remove toolbar pointer events

--- src/components/commons/Toolbar.jsx
+++ src/components/commons/Toolbar.jsx
@@ -1,6 +1,6 @@
 const Toolbar = ({ kassenzeichen, anschlussgrad, bezeichnung }) => {
   return (
-    <div className="absolute bottom-0 text-lg left-0 bg-white w-fit px-2 z-[9999]">
+    <div className="absolute bottom-0 text-lg left-0 bg-white w-fit px-2 z-[9999] pointer-events-none">
       {kassenzeichen && "Kassenzeichen: " + kassenzeichen + "::" + bezeichnung}
       {kassenzeichen && anschlussgrad && " - "}
       {anschlussgrad && anschlussgrad}
